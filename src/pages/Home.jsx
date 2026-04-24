b476c2781bf88a688cde04354cb1c5b4582b50d7
dakh524 <dakhedusolution@gmail.com>
2026-04-24 12:46:08 +0530

    fix: resolve unterminated regular expression error caused by mismatched divs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -673,14 +673,13 @@ export default function Home() {
       </div>
     </div>
   </div>
-    </div>
-  </div>
 )}
 
 </div>
 </div>
 </div>
 </section>
+
 
 {/* Application Modal */}
 {isOpportunityModalOpen && (
